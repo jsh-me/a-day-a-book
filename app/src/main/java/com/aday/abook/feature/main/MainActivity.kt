c454fc834f0a6028b5584c689ab99dd951c1e94e
jsh <ppm_it@naver.com>
2020-06-12 16:41:10 +0900

    main view model added, single live event listener added

--- app/src/main/java/com/aday/abook/feature/main/MainActivity.kt
+++ app/src/main/java/com/aday/abook/feature/main/MainActivity.kt
@@ -2,17 +2,28 @@ package com.aday.abook.feature.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.aday.abook.BaseApplication
 import com.aday.abook.R
+import com.aday.abook.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(){
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         injectComponent()
-
+        setupDatabinding()
+        observeViewModel()
     }
 
     private fun injectComponent(){
@@ -21,5 +32,15 @@ class MainActivity : AppCompatActivity(){
             .activity(this)
             .build()
             .inject(this)
+    }
+
+    private fun setupDatabinding(){
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mViewModel = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
+        mBinding.mainViewModel = mViewModel
+    }
+
+    private fun observeViewModel(){
+
     }
 }
--- app/src/main/java/com/aday/abook/feature/main/MainViewModel.kt
+++ app/src/main/java/com/aday/abook/feature/main/MainViewModel.kt
@@ -2,8 +2,32 @@ package com.aday.abook.feature.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.aday.abook.mvvm.SingleLiveEvent
 import com.aday.core.api.usecase.GetSearchBookUseCase
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(application: Application,
-                                        private val getSearchBookUseCase: GetSearchBookUseCase) : AndroidViewModel(application)
+                                        private val getSearchBookUseCase: GetSearchBookUseCase) : AndroidViewModel(application){
+
+    val mDetailClicked: SingleLiveEvent<Void> = SingleLiveEvent()
+    val mAddBookClicked: SingleLiveEvent<Void> = SingleLiveEvent()
+    val mAddWord: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun initData(){
+        //RoomDB
+    }
+
+    fun addWordClicked(){
+        mAddWord.value = true
+    }
+
+    fun addBookClicked(){
+        mAddBookClicked.call()
+    }
+
+    fun detailClicked(){
+        mDetailClicked.call()
+    }
+
+}
