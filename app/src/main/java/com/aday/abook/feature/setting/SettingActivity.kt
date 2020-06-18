350687f75e105fcd46ab1f8b887d507fe21a50da
jsh <ppm_it@naver.com>
2020-06-19 00:56:34 +0900

    setting xml, activity added

---
+++ app/src/main/java/com/aday/abook/feature/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.aday.abook.feature.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aday.abook.R
+
+class SettingActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
